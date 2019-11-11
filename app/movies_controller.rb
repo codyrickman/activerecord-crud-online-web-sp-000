cb667769a7b13a9184dce90880c9817a5e3c4c3f
Cody Rickman <cody@creativemindstudios.com>
2019-11-11 02:02:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -91,7 +91,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  movie = Movie.where(title: That One Where the Guy Kicks Another Guy Once).first
+  movie = Movie.where(title: "That One Where the Guy Kicks Another Guy Once").first
   movie.destroy
 end
 
